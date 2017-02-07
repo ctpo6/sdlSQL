1837a454359eb90c77064c6d3c137524e5f40266
Yuri Epstein <yuri.epstein@gmail.com>
2017-02-07 19:32:36 +0300

    implementing SQL executor...

--- src/sql_executor.cpp
+++ src/sql_executor.cpp
@@ -1,8 +1,107 @@
 #include "sql_executor.hpp"
 
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
 
 int SqlExecutor::walk()
 {
+    cout << "\nwalk:\n";
+
+    if (emit_.empty())
+        return 1;
+
+    auto ite = emit_.end() - 1;
+    if (ite->op_ != EmitOp::SELECT)
+        return 1;
+
+    int n_select = ite->int_;
+    if (n_select < 1)
+        return 1;
+    cout << "SELECT " << n_select << endl;
+    --ite;
+
+    int n_top = 0;
+    auto itb = emit_.begin();
+    if (itb->op_ == EmitOp::TOP) {
+        n_top = itb->int_;
+        ++itb;
+        cout << "TOP " << n_top << endl;
+    }
+
+    bool f_select_all = false;
+
+    for (int i = 0; i < n_select; ++i) {
+        assert(!f_select_all);  // can be set only once
+        assert(itb->op_ == EmitOp::SELECT_ALL || itb->op_ == EmitOp::FIELD);
+        if (itb->op_ == EmitOp::SELECT_ALL) {
+            f_select_all = true;
+            cout << "SELECT_ALL" << endl;
+        }
+        else {
+            cout << "FIELD " << itb->name1_ << ' ' << itb->name2_ << endl;
+        }
+        ++itb;
+
+        if (itb->op_ == EmitOp::AS) {
+            cout << "AS " << itb->name2_ << endl;
+            ++itb;
+        }
+    }
+    // itb now points to the begin of FROM part
+
+    // ORDER BY
+    if (ite->op_ == EmitOp::ORDER_BY) {
+        if (walk_order_by(ite, ite) != 0) {
+            return 1;
+        }
+    }
+
+    // WHERE
+    if ()
+
+    return 0;
+}
+
+
+int SqlExecutor::walk_order_by(
+        const EmitRecordContainer::iterator start,
+        EmitRecordContainer::iterator& next_part)
+{
+    assert(start->op_ == EmitOp::ORDER_BY);
+    cout << "ORDER BY" << endl;
+
+    auto it = start - 1;
+    while (it->op_ != EmitOp::WHERE && it->op_ != EmitOp::FROM) {
+        --it;
+    }
+    assert(start - it > 1);
+
+    next_part = it;
+
+    ++it;
+    while (it->op_ != EmitOp::ORDER_BY) {
+        assert(it->op_ == EmitOp::FIELD || it->op_ == EmitOp::NAME);
+        if (it->op_ == EmitOp::NAME) {
+            cout << "NAME " << it->name2_ << endl;
+        }
+        else {
+            cout << "FIELD " << it->name1_ << '.' << it->name2_ << endl;
+        }
+        ++it;
+
+        if (it->op_ == EmitOp::ASC) {
+            cout << "ASC" << endl;
+            ++it;
+        }
+        else if (it->op_ == EmitOp::DESC) {
+            cout << "DESC" << endl;
+            ++it;
+        }
+    }
+
     return 0;
 }
 
--- src/sql_executor.hpp
+++ src/sql_executor.hpp
@@ -7,25 +7,25 @@
 class SqlExecutor
 {
 public:
-    enum EmitOp
+    enum class EmitOp
     {
-        EMOP_AS,
-        EMOP_ASC,
-        EMOP_COMPARISON,
-        EMOP_DESC,
-        EMOP_FIELD,
-        EMOP_FROM,
-        EMOP_JOIN,
-        EMOP_NAME,
-        EMOP_NUMBER,
-        EMOP_OPERATOR,
-        EMOP_ORDER_BY,
-        EMOP_STRING,
-        EMOP_TABLE,
-        EMOP_TOP,
-        EMOP_SELECT,
-        EMOP_SELECT_ALL,
-        EMOP_WHERE,
+        AS,
+        ASC,
+        COMPARISON,
+        DESC,
+        FIELD,
+        FROM,
+        JOIN,
+        NAME,
+        NUMBER,
+        OPERATOR,
+        ORDER_BY,
+        STRING,
+        TABLE,
+        TOP,
+        SELECT,
+        SELECT_ALL,
+        WHERE,
     };
 
     void emit(EmitOp op);
@@ -43,11 +43,13 @@ private:
         int int_;               // for int values
         std::string string_;    // for string values
 
-        // column or table reference: name2_.name1_
-        // alias: name1_
+        // column or table reference: name1_.name2_
+        // alias: name2_
         std::string name1_;
         std::string name2_;
     };
+
+    using EmitRecordContainer = std::deque<EmitRecord>;
 
     static EmitRecord make_emit_record(EmitOp op);
     static EmitRecord make_emit_record(EmitOp op, int param);
@@ -56,9 +58,13 @@ private:
                                        const std::string& name1,
                                        const std::string& name2);
 
-    int walk();
 
-    std::deque<EmitRecord> emit_;
+    int walk();
+    int walk_order_by(const EmitRecordContainer::iterator start,
+                      EmitRecordContainer::iterator& next_part);
+    int walk_where();
+
+    EmitRecordContainer emit_;
 };
 
 #endif
